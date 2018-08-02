1450e792b65c80143f8302bdc9b26b49f4e68fc1
Richard Ma <richard.ma.19850509@gmail.com>
2018-08-02 22:17:13 +0800

    添加标识符类IdToken

---
+++ src/idToken.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from stoneToken import StoneToken
+
+class IdToken(StoneToken):
+    def __init__(self, line, id):
+        super(IdToken, self).__init__(line)
+        self.text = id
+
+    def isIdentifier(self):
+        return True
+
+    def getText(self):
+        return self.text
+
+if __name__ == '__main__':
+    it = IdToken(1, 'identifier')
+
+    assert it.isNumber() == False
+    assert it.isString() == False
+    assert it.isIdentifier() == True
+    assert it.getText() == "identifier"
