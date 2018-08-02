4b962188fff0744fe5a7b2080484816217f4034b
Richard Ma <richard.ma.19850509@gmail.com>
2018-08-02 18:26:03 +0800

    添加测试和使用方法样例

--- src/stoneException.py
+++ src/stoneException.py
@@ -1,3 +1,15 @@
 class StoneException(Exception):
-    def __init__(self):
+    def __init__(self, errmsg):
         super(StoneException, self).__init__()
+        self.errmsg = errmsg
+
+    def __str__(self):
+        return self.errmsg
+
+# Testing and Usage
+if __name__ == '__main__':
+    try:
+        raise StoneException("This is a StoneException")
+    except StoneException as e:
+        print(e)
+        raise
