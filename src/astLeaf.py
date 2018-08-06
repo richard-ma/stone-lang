9b68046938656d06f0bccbafe0615ae1d0e09c8a
Richard Ma <richard.ma.19850509@gmail.com>
2018-08-06 22:15:48 +0800

    添加单元测试

--- src/astLeaf.py
+++ src/astLeaf.py
@@ -5,24 +5,37 @@ from astree import ASTree
 from stoneToken import StoneToken
 
 class ASTLeaf(ASTree):
-    def __ini__(self, token):
+    def __init__(self, t):
         self.empty = list()
-        self.token = token
+        self.t = t
 
     def child(self, i):
-        pass
+        # throw Exception
+        return None
 
     def numChildren(self):
         return 0
 
     def children(self):
+        # return empty list
+        # OR throw Exception
         return empty
 
     def __str__(self):
-        return self.token.getText()
+        return self.t.getText()
 
     def location(self):
-        return "at line " + self.token.getLineNumber()
+        return "at line " + str(self.t.getLineNumber())
 
     def token(self):
-        return self.token
+        return self.t
+
+if __name__ == '__main__':
+    from numToken import NumToken
+    token = NumToken(1, 33)
+    leaf = ASTLeaf(token)
+
+    assert leaf.numChildren() == 0
+    assert str(leaf) == "33"
+    assert leaf.location() == "at line %d" % (1)
+    assert leaf.token() == token
