bde8203cc4f4ff18b570e0a652fe02c2ba3c7711
Richard Ma <richard.ma.19850509@gmail.com>
2018-08-09 11:45:42 +0800

    未完成

--- src/parser.py
+++ src/parser.py
@@ -3,13 +3,20 @@
 
 from parseException import ParseException
 from astList import ASTList
+from astLeaf import ASTLeaf
 
 class Element():
     def parse(self, lexer, res):
-        pass
+        return None
 
     def match(self, lexer):
-        pass
+        return None
+
+if __name__ == '__main__':
+    e = Element()
+    assert e.parse() is None
+    assert e.match() is None
+    exit()
 
 class Tree(Element):
     def __init__(self, parser):
@@ -62,9 +69,157 @@ class Repeat(Element):
 
 class AToken(Element):
     def __init__(self, t):
+        self.factory = Factory()
         if t is None:
-            t = ASTLeaf.__name__
+            t = eval(ASTLeaf.__name__)
             factory = self.factory.get(t, StoneToken.__name__)
+
+    def parse(self, lexer, res):
+        t = lexer.read()
+        if self.test(t):
+            leaf = self.factory.make(t)
+            res.add(leaf)
+        else:
+            raise parseException(t)
+
+    def match(self, lexer):
+        return self.test(lexer.peek(0))
+
+    def test(self, t):
+        pass
+
+class AIdToken(AToken):
+    def __init__(self, t, r):
+        super(AIdToken, self).__init__(t)
+        self.reserved = r if r != None else dict()
+
+    def test(self, t):
+        return t.isIdentifier() and not t.getText() in self.reserved
+
+class ANumToken(AToken):
+    def __init__(self, t):
+        super(ANumToken, self).__init__(t)
+
+    def test(self, t):
+        return t.isNumber();
+
+class AStrToken(AToken):
+    def __init__(self, t):
+        super(AStrToken, self).__init__(t)
+
+    def test(self, t):
+        return t.isString()
+
+class Leaf(Element):
+    def __init__(self, pat):
+        self.tokens = pat
+
+    def parse(self, lexer, res):
+        t = lexer.read()
+        if t.isIdentifier():
+            for token in self.tokens:
+                if token == t.getText():
+                    self.find(res, t)
+                    return
+
+        if len(tokens) > 0:
+            raise ParseException(tokens[0] + " expected.", t)
+        else:
+            raise ParseException(t)
+
+    def find(self, res, t):
+        res.add(ASTLeaf(t))
+
+    def match(self, lexer):
+        t = lexer.peek(0)
+        if t.isIdentifier():
+            for token in self.tokens:
+                if token == t.getText():
+                    return True
+
+        return False
+
+class Skip(Leaf):
+    def __init__(self, t):
+        super(Skip, self).__init__(t)
+
+    def find(self, res, t):
+        pass
+
+class Precedence:
+    def __init__(self, v, a):
+        self.value = v
+        self.leftAssoc = a
+
+# Extend dict class
+# https://stackoverflow.com/questions/2328235/pythonextend-the-dict-class
+class Operators(dict):
+    LEFT = True
+    RIGHT = False
+
+    def __init__(self, *args, **kw):
+        super(Operators, self).__init__(*args, **kw)
+
+    def add(name, perc, leftAssoc):
+        super(Operators, self).__setitem__(name, Precedence(prec, leftAssoc))
+
+class Expr(Element):
+    def __init__(self, clazz, exp, m):
+        self.factory = Factory.getForASTList(clazz)
+        self.ops = m
+        self.factor = exp
+
+    def parse(self, lexer, res):
+        right = self.factor.parse(lexer)
+        prec = None
+        while (prec = nextOperator(lexer)) != None:
+            right = doShift(lexer, right, prec.value)
+
+        res.add(right)
+
+    def doShift(self, lexer, left, prec):
+        l = list()
+        l.append(left)
+        l.append(ASTLeaf(lexer.read()))
+        right = factor.parse(lexer)
+        while (n = nextOperator(lexer)) != None and rightIsExpr(prec, n):
+            right = doShift(lexer, right, n.value)
+        l.append(right)
+        return self.factory.make(l)
+
+    def nextOperator(self, lexer):
+        t = lexer.peek(0)
+        if t.isIdentifier():
+            return ops.get(t.getText())
+        else:
+            return None
+
+    def rightIsExpr(self, prec, nextPrec):
+        if nextPrec.leftAssoc:
+            return prec < nextPrec.value
+        else:
+            return prec <= nextPrec.value
+
+    def match(self, lexer):
+        return self.factory.match(lexer)
+
+class Factory():
+    def make0(self):
+        pass
+
+    def make(self, arg):
+        try:
+            return make0(arg)
+        except Exception as e:
+            raise e
+
+    def getForASTList(self, clazz):
+        pass
+        #f = get(clazz, list)
+        #if f == None:
+            #f = Factory()
+
+
 
 class Parser():
     pass
