345048c6186491e34882a4c87d01fea102c88cdf
Richard Ma <richard.ma.19850509@gmail.com>
2018-08-13 10:07:13 +0800

    add ASTree testcase

--- tests/astree_test.py
+++ tests/astree_test.py
@@ -9,19 +9,38 @@ class TestASTree(unittest.TestCase):
     """Test case docstring."""
 
     def setUp(self):
+        self.child1 = ASTree()
+        self.child2 = ASTree()
         self.astree = ASTree()
 
     def tearDown(self):
         pass
 
+    def append_childs(self):
+        self.astree.add(self.child1)
+        self.astree.add(self.child2)
+
     def test_append(self):
-        child = ASTree()
-        self.astree.add(child)
-        assert self.astree.numChildren() == 1
-        assert self.astree.child(0) == child
+        self.astree.add(self.child1)
+        self.assertEqual(self.astree.numChildren(), 1)
 
     def test_child(self):
-        pass
+        self.append_childs()
+        self.assertEqual(self.astree.child(0), self.child1)
+        self.assertEqual(self.astree.child(1), self.child2)
+
+    def test_numChildren(self):
+        self.append_childs()
+        self.assertEqual(self.astree.numChildren(), 2)
+
+    def test_location(self):
+        self.assertIsNone(self.astree.location())
+
+    def test_children(self):
+        self.append_childs()
+        self.assertListEqual(
+                self.astree.children(),
+                [self.child1, self.child2])
 
 if __name__ == '__main__':
     unittest.main()
