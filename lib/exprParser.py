f92f9d7d4f0420e149d9cbc8b2634abda6e35e77
Richard Ma <richard.ma.19850509@gmail.com>
2018-08-21 15:16:22 +0800

    debug to run

--- lib/exprParser.py
+++ lib/exprParser.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from lexer import *
-from parseException import *
-from astLeaf import ASTLeaf
-from binaryExpr import BinaryExpr
-from numberLiteral import NumberLiteral
+from lib.lexer import *
+from lib.parseException import *
+from lib.astLeaf import ASTLeaf
+from lib.binaryExpr import BinaryExpr
+from lib.numberLiteral import NumberLiteral
 
 class ExprParser():
     def __init__(self, p):
@@ -57,7 +57,7 @@ if __name__ == '__main__':
 
     # 合法输入
     print('parsing expression.stone')
-    with open("../samples/expression.stone", 'r') as f:
+    with open("samples/expression.stone", 'r') as f:
         reader = LineReader(f)
         lexer = Lexer(reader)
         #token = lexer.read()
@@ -72,7 +72,7 @@ if __name__ == '__main__':
 
     # 非法输入 应该产生ParseException
     print('parsing expression_parseexception.stone')
-    with open("../samples/expression_parseexception.stone", 'r') as f:
+    with open("samples/expression_parseexception.stone", 'r') as f:
         reader = LineReader(f)
         lexer = Lexer(reader)
         #token = lexer.read()
